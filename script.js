31b9abb4f765eb212a043d61ed49aa5eb8871814
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-07-06 10:52:46 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function shortcut(s1, s2) {
   // your code here
+	return s1.charCodeAt(0)+s2.charCodeAt(0);
 }
 
-// Do not change the code below.
+ Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
